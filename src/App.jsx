e4d9cda0db2331047b4d2f65ac995b54dd890523
Aleksander White <zander1116@aol.com>
2023-10-16 18:31:12 -0700

    started a nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import React from 'react'
+import SideBar from './components/SideBar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 function App() {
-
   return (
-    <>
-
-    </>
+    <div>
+      <BrowserRouter>
+        <SideBar />
+        <Routes>
+          <Route path='/' />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
