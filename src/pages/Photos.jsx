fa9688c570c43f75dfba06f316dbb9171801c741
Aleksander White <zander1116@aol.com>
2023-10-17 21:25:22 -0700

    added photos

--- src/pages/Photos.jsx
+++ src/pages/Photos.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { PhotoData } from './PhotosData'
 
 function Photos() {
+
+    const photoList = PhotoData.map((photo, index) => {
+        return (
+            <img src={photo.img} key={index} alt='' className='photo'/>
+        )
+    })
+
   return (
-    <div className='photos'>
-        <h1>Photos</h1>
+    <div className='flex flex-wrap m-3 pr-5 justify-around w-screen'>
+        {
+            photoList
+        }
     </div>
   )
 }
---
+++ src/pages/PhotosData.jsx
@@ -0,0 +1,58 @@
+import santaBoy from '../assets/santa_boy.png'
+import santaWave from '../assets/santa_wave.jpg'
+import santaCane from '../assets/santa_cane.jpg'
+import santaChair from '../assets/santa_chair.jpg'
+import santaFamily from '../assets/santa_family.jpg'
+import santaGirl from '../assets/santa_girl.jpg'
+import santaMrs from '../assets/santa_mrs.jpg'
+import santaCookie from '../assets/santa_cookie.png'
+import santaFace from '../assets/santa_head.png'
+import santaMother from '../assets/santa_mom.png'
+import santaSit from '../assets/santa_sit.png'
+import santaSmile from '../assets/santa_smile.png'
+import santaShh from '../assets/santa_shh.png'
+
+
+
+export const PhotoData = [
+    {
+        img: santaBoy
+    },
+    {
+        img: santaWave
+    },
+    {
+        img: santaCane
+    },
+    {
+        img: santaChair
+    },
+    {
+        img: santaFamily
+    },
+    {
+        img: santaGirl
+    },
+    {
+        img: santaMrs
+    },
+    {
+        img: santaCookie
+    },
+    {
+        img: santaFace
+    },
+    {
+        img: santaMother
+    },
+    {
+        img: santaSit
+    },
+    {
+        img: santaSmile
+    },
+    {
+        img: santaShh
+    }
+]
+
